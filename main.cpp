3ae10f997cf722f75672c30cbc68d75fd17f8eaa
Endless Arch <37617947+EndlessArch@users.noreply.github.com>
2020-08-23 00:11:15 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 int main(int argc, char * argv[]) {
    // 3
    
-   // 우선 껏다 키기
-   system("sudo ifconfig en0 down");
-   system("sudo ifconfig en0 up");
-   
    std::string MAC = "A1:AA:A2:B2:BB:B1";
    if(argc - 1) MAC = argv[1];
 
    // 그 담에 MAC주소 바꿔버리기
    system(("sudo ifconfig en0 ether " + MAC).c_str());
    
+   system("sudo ifconfig en0 down");
+   system("sudo ifconfig en0 up");
+
+
    return 0;
 }
