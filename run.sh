d8876d5ad84ca0517a9776e89a3f3f53ccc12b1f
Endless Arch <37617947+EndlessArch@users.noreply.github.com>
2020-08-23 00:48:25 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 echo "실행~"
 
 STR=""
+DEV="en0" # LAN Device of Mac
 FILE=TARGET_MAC_ADDR.txt
 if [ -f "$FILE" ]; then
     # 4
@@ -10,16 +11,16 @@ if [ -f "$FILE" ]; then
     echo $STR
 else
     # 4
-    echo "기본 MAC 주소는 A1:AA:A2:B2:BB:B1. 원하는 MAC 주소가 있다면 $FILE 생성 후 MAC 주소만 적을 것(검사는 없음)"
+    echo "기본 MAC 주소는 A1:AA:A2:B2:BB:B1. 원하는 MAC 주소가 있다면 $FILE 생성 후 MAC 주소만 적을 것(검사는 없으니 주의)"
     STR="A1:AA:A2:B2:BB:B1"
 fi
 
 for ((i=1; i< 10; i++)); do
     #4
-    sudo ifconfig en0 ether $STR
+    sudo ifconfig $DEV ether $STR
 done
 
 echo "이더넷 장치를 껐다 킴"
-sudo ifconfig en0 down
+sudo ifconfig $DEV down
 
-sudo ifconfig en0 up
+sudo ifconfig $DEV up
