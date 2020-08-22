a5aa2b25e6a84473d6728399f7e1996bac33e78d
Endless Arch <37617947+EndlessArch@users.noreply.github.com>
2020-08-23 00:43:07 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 echo "실행~"
-PROGRAM=MACChanger
-if [ ! -f "$PROGRAM" ]; then
-    # 4
-    echo "$PROGRAM 프로그램 못찾음."
-    cmake CMakeLists.txt -DCMAKE_BUILD_TYPE=Release
-    make
-fi
 
+STR=""
 FILE=TARGET_MAC_ADDR.txt
 if [ -f "$FILE" ]; then
     # 4
-    echo "$FILE"
-    sudo ./MACChanger $FILE
+    STR=$(head -n 1 $FILE)
+    echo $STR
 else
     # 4
-    echo "기본 MAC 주소는 A1:AA:A2:B2:BB:B1.\n원하는 MAC 주소가 있다면 $FILE 생성 후 MAC 주소만 적을 것(따로 검사는 안함)"
-    sudo ./MACChanger
+    echo "기본 MAC 주소는 A1:AA:A2:B2:BB:B1. 원하는 MAC 주소가 있다면 $FILE 생성 후 MAC 주소만 적을 것(검사는 없음)"
+    STR="A1:AA:A2:B2:BB:B1"
 fi
+
+for ((i=1; i< 10; i++)); do
+    #4
+    sudo ifconfig en0 ether $STR
+done
+
+echo "이더넷 장치를 껐다 킴"
+sudo ifconfig en0 down
+
+sudo ifconfig en0 up
